8050d298cab55aa132d356c35dfd5847564fac89
DangQuangTien <dangquangtien16102002@gmail.com>
2026-01-07 20:34:01 +0700

    update DB

---
+++ DataLabelingSystem/DataLabeling.DAL/Migrations/20260107133252_AddErrorTypeColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLabeling.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddErrorTypeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ErrorType",
+                table: "LabelTasks",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorType",
+                table: "LabelTasks");
+        }
+    }
+}
